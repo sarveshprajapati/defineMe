31262461903c5f010b1e4ac3011c2867cd87bf7d
Sarvesh Prajapati <thesarveshprajapati@hotmail.com>
2021-05-07 10:45:31 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log(`
+        Hey there, I am Sarvesh Prajapati 🙄.
+
+        A learning-driven developer with 2+ years of experience in programming
+        with an eagerness to adapt the professionalism.
+
+        You can find me via following links ↴:
+        Email    : thesarveshprajapati@hotmail.com
+        LinkedIn : https://www.linkedin.com/in/sarveshprajapati/
+        GitHub   : https://github.com/sarveshprajapati
+        Twitter  : https://twitter.com/iamthesarvesh
+        Medium   : https://medium.com/@thesarveshprajapati
+
+        So yeah, that's pretty much about me :)
+    `);
+
